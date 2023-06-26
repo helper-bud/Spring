e74cd53971ef84261cb391d9b6af8eff119346c5
Documentation Duh :3 <96073081+dr-croc@users.noreply.github.com>
2023-06-26 17:42:28 +0600

    Create 3. Bike.java

---
+++ Telesko/Annotation Based Configuration/3. Bike.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bike implements Vehicle {
+    public void drive() {
+        System.out.println("Bhag raha hay..");
+    }
+
+}
