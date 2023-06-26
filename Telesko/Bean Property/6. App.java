9b1e3214c2a5043c1a769bfd190b743ba10d9f26
Documentation Duh :3 <96073081+dr-croc@users.noreply.github.com>
2023-06-26 19:17:19 +0600

    Create 6. App.java

---
+++ Telesko/Bean Property/6. App.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main( String[] args )
+    {
+        ApplicationContext applicationContext  = new ClassPathXmlApplicationContext("spring.xml");
+
+        Tyre t  = (Tyre) applicationContext.getBean("tyre");
+        System.out.println(t);
+    }
+}
