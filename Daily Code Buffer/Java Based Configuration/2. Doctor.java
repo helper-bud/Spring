10a16749b1bb76d694bb07f91b0ca17068653406
Documentation Duh :3 <96073081+dr-croc@users.noreply.github.com>
2023-07-01 14:17:49 +0600

    Create 2. Doctor.java

---
+++ Daily Code Buffer/Java Based Configuration/2. Doctor.java
@@ -0,0 +1,7 @@
+import org.springframework.stereotype.Component;
+@Component
+public class Doctor  implements  Staff{
+    public  void assist(){
+        System.out.println("doctor here");
+    }
+}
